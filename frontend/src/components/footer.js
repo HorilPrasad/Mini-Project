62227d5e383dd475585d9bee0dee4568f81dc549
Arya_Shanish <70702833+Aryashanish@users.noreply.github.com>
2023-03-20 01:10:44 +0530

    Add files via upload

---
+++ frontend/src/components/footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+    return (
+        <>
+            <footer className="footer">
+                <br></br>
+                <p class="text-center">© 2022-2023 <spam>Easy Way</spam>, Inc. · Privacy · Terms & Condition</p>
+            </footer>
+        </>
+    );
+};
+
+export default Footer;
